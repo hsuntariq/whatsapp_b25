b842e41796ca9a2d6f85f59535e32ab974fd377c
unknown <hsuntariq@gmail.com>
2026-03-06 14:25:20 +0500

    prev

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,15 @@ import { authRouter } from "./routes/userRoutes.js";
 import { errorHandler } from "./middlewares/errorMiddlewares.js";
 import { connectDB } from "./database/connect.js";
 dotenv.config();
+
+// For ESM (import syntax)
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "1.0.0.1", "8.8.8.8"]);
+
+// // OR for CommonJS (require syntax)
+// const dns = require("node:dns/promises");
+// dns.setServers(["1.1.1.1", "1.0.0.1", "8.8.8.8"]);
+
 const app = express();
 
 connectDB();
